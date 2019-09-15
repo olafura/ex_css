bb3678691b32d69922adb41b18e04a46dbd191e7
Olafur Arason <olafura@olafura.com>
2019-09-15 16:38:32 -0400

    Various fixes to pass parsing tests

--- test/ex_css_test.exs
+++ test/ex_css_test.exs
@@ -2,7 +2,18 @@ defmodule ExCssTest do
   use ExUnit.Case
   doctest ExCss
 
-  test "greets the world" do
-    assert ExCss.hello() == :world
+  test "simple css" do
+    ExCss.parse_css("""
+    p.something {
+      color: red;
+    }
+    """)
+  end
+
+  test "spreadsheet" do
+    TestHelper.parse_json("test/css-parsing-tests/stylesheet.json")
+    |> Enum.map(fn [stylesheet, check] ->
+      assert ExCss.parse_stylesheet(stylesheet) |> TestHelper.result_to_list() == check
+    end)
   end
 end

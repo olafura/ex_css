a900c41f9dac9a7ddf3803d5c362cf0e6f4c45a2
Olafur Arason <olafura@olafura.com>
2021-05-02 21:17:15 +0200

    decent stringifier

--- test/ex_css_test.exs
+++ test/ex_css_test.exs
@@ -13,7 +13,8 @@ defmodule ExCSSTest do
   test "spreadsheet" do
     TestHelper.parse_json("test/css-parsing-tests/stylesheet.json")
     |> Enum.map(fn [stylesheet, check] ->
-      assert ExCSS.Parser.parse_stylesheet(stylesheet) |> TestHelper.result_to_list() == check
+      real = ExCSS.Parser.parse_stylesheet(stylesheet) |> TestHelper.result_to_list()
+      assert real == check, "stylesheet: #{stylesheet}\nc: #{inspect(check)}\nr: #{inspect(real)}"
     end)
   end
 end
